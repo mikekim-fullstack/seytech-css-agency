81a56bed08d61d9d40971079cc737c53618ec2f1
mikekim-fullstack <mikekim.fullstack@gmail.com>
2022-10-06 17:26:12 -0400

    radio image outline is not working

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAW_vth6QKN6YweuXtpk2gDMtDJ2I9QhWY",
+    authDomain: "agency-e8817.firebaseapp.com",
+    projectId: "agency-e8817",
+    storageBucket: "agency-e8817.appspot.com",
+    messagingSenderId: "550415866275",
+    appId: "1:550415866275:web:565335b713807828ade216"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
